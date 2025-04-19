b4d4bfc89ffe718950560d15542529f3b2235af8
Burhanuddin Vora <burhanuddin.vora@gmail.com>
2025-04-20 02:02:43 +0530

    modfied routing

--- grivax/src/components/header.tsx
+++ grivax/src/components/header.tsx
@@ -69,8 +69,8 @@ export default function Header() {
       name: "Courses",
       href: isAuthenticated && userId ? `/courses/${userId}` : "/login",
     },
-    { name: "Assessments", href: "/assessments" },
-    { name: "Dashboard", href: `/dashboard/${userId}` },
+    { name: "Assessments", href: isAuthenticated && userId ? `/assessments/${userId}` : "/login" },
+    { name: "Dashboard", href: isAuthenticated && userId ? `/dasboard/${userId}` : "/login" },
   ]
 
   const menuItems = [
@@ -79,7 +79,7 @@ export default function Header() {
       items: [
         {
           name: "Dashboard",
-          href: "/dashboard/${userId}",
+          href: isAuthenticated && userId ? `/dashboard/${userId}` : "/login",
           icon: <User className="h-4 w-4" />,
         },
         {
