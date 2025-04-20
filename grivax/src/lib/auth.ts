87e1c545090a35e0f7d762b95ce41073c7595927
Burhanuddin Vora <burhanuddin.vora@gmail.com>
2025-04-21 02:41:36 +0530

    added auth

--- grivax/src/lib/auth.ts
+++ grivax/src/lib/auth.ts
@@ -143,6 +143,7 @@ export const authConfig: NextAuthOptions = {
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
 };
 
 export async function loginIsRequiredServer() {
